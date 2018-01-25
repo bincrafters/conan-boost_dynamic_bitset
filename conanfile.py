e277605cca1e3f74daa4443b3376e1392300786e
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:42 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostDynamic_BitsetConan(ConanFile):
     name = "boost_dynamic_bitset"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_dynamic_bitset"
-    
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["dynamic_bitset"]
     is_header_only = True
 
