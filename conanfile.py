1f6159103ffd9d6c4f6922eb8618cb5d8e781096
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:11:25 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,13 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostDynamic_BitsetConan(base.BoostBaseConan):
     name = "boost_dynamic_bitset"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_dynamic_bitset"
-    lib_short_names = ["dynamic_bitset"]
-    header_only_libs = ["dynamic_bitset"]
-    cycle_group = "boost_cycle_group_c"
-    b2_requires = ["boost_cycle_group_c"]
+    version = "1.70.0"
